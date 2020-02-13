f9dd67947927be1fadbec00a4f9adae76cad0515
Unai Foncea <unaifon@gmail.com>
2020-02-13 12:03:13 +0100

    Column

--- BrowseLib/Column.cs
+++ BrowseLib/Column.cs
@@ -8,5 +8,28 @@ namespace BrowseLib
 {
     public class Column
     {
+        public string name;
+        public string type;
+        public List<Object> columns;
+        public Column(string name, string type)
+        {
+            this.name = name;
+            this.type = type;
+            if (type == "TEXT")
+            {
+                List<string> columns = new List<string>();
+            }
+            if (type == "INT")
+            {
+                List<int> columns = new List<int>();
+            }
+            if (type == "DOUBLE")
+            {
+                List<double> columns = new List<double>();
+            }
+        }
+
     }
 }
+
+
