535192805434388b467abdec13ec00df46425fa9
Ander Pascual González <apascual047@ikasle.ehu.eus>
2020-02-13 13:44:23 +0200

    Browse Class

--- BrowseLib/Browse.cs
+++ BrowseLib/Browse.cs
@@ -8,5 +8,25 @@ namespace BrowseLib
 {
     class Browse
     {
+        List<Database> databases;
+
+        public Browse()
+        {
+            databases = new List<Database>();
+        }
+
+        public void loadDatabases()
+        {
+
+        }
+
+        public void addDatabase(Database database) {
+            databases.Add(database);
+        }
+
+        public void deleteDatabase(Database database)
+        {
+            databases.Remove(database);
+        }
     }
 }
