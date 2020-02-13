14f14b0e807467fc19c31b3d16e91045d2b57027
Borja Rey <borja300@gmail.com>
2020-02-13 11:47:30 +0100

    table

--- BrowseLib/Table.cs
+++ BrowseLib/Table.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,61 @@ namespace BrowseLib
 {
     class Table
     {
+        private string name;
+
+       private List<Column> columns;
+
+        public Table(string n) {
+            name = n;
+            columns = new List<Column>();
+         }
+
+        public void addColumn(Column c) {
+            columns.Add(c);
+        }
+
+        public void dropColumn(Column c)
+        {
+            columns.Remove(c);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
