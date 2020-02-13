189064c6da2af9335f2bf2decaad91d460632401
Javier Ortiz <javierortizdiez@gmail.com>
2020-02-13 12:11:38 +0100

    Database class

--- BrowseLib/Database.cs
+++ BrowseLib/Database.cs
@@ -8,5 +8,21 @@ namespace BrowseLib
 {
     public class Database
     {
+         private string name;
+         private List<Table> tables;
+
+        public Database(string name) {
+            this.name = name;
+            tables = new List<Table>();
+         }
+
+        public void addTable(Table table) {
+            tables.Add(table);
+        }
+
+        public void dropTable(Table table)
+        {
+            tables.Remove(table);
+        }
     }
 }
