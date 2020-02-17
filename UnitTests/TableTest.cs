0fba43e2ad4968a5ce3ed25d97db3a4f6f2c08f2
Borja Rey <borja300@gmail.com>
2020-02-17 11:57:30 +0100

    unitTestTable

---
+++ UnitTests/TableTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowseLib;
+
+namespace UnitTests
+{
+    /// <summary>
+    /// Summary description for UnitTest2
+    /// </summary>
+    [TestClass]
+    public class TableTest
+    {
+        public void Test() {
+
+            Table table = new Table("Tabla1");
+            Table t = table;
+            Column c1 = new Column("name", "TEXT");
+            Column c2 = new Column("age", "INT");
+            Column c3 = new Column("date", "DOUBLE");
+            table.addColumn(c1);
+            table.addColumn(c2);
+            table.addColumn(c3);
+            table.dropColumn(c2);
+            Assert.AreEqual(table, t);
+
+
+        }
+       
+
+
+    }
+}
---
+++ UnitTests/UnitTest2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowseLib;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Table table = new Table("Tabla1");
+            
+            Column c1 = new Column("name", "TEXT");
+            Column c2 = new Column("age", "INT");
+            Column c3 = new Column("date", "DOUBLE");
+            table.addColumn(c1);
+            table.addColumn(c2);
+            table.addColumn(c3);
+            table.dropColumn(c2);
+            
+
+            
+        }
+    }
+}
