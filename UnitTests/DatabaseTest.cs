a60bc84f4f325ea2a002c753b4cd768025092e9e
Javier Ortiz <javierortizdiez@gmail.com>
2020-02-17 11:33:49 +0100

    Database test

---
+++ UnitTests/DatabaseTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowseLib;
+
+namespace UnitTests
+{
+    public class DatabaseTest
+    {
+            private readonly string testname;
+            public void Database()
+            {
+                Table table = new Table("test-table");
+                Database db = new Database("test-db");
+                db.addTable(table);
+                db.dropTable(table);
+                db.updateName(testname);
+
+
+                //save Database
+                //load database
+                //Assert.AreEqual(db.)
+            }
+        
+    }
+}
