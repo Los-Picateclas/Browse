d3f0c22d5f9e3b8b9ae81766d03f5ce453793b21
Unai Foncea <unaifon@gmail.com>
2020-02-20 12:27:30 +0100

    unitTestColumn???

---
+++ UnitTests/UnitTestColumn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowseLib;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTestColumn
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Column columna1 = new Column("prueba1", "TEXT");
+            columna1.insertTEXTtoTable("Olaa K asEE");
+            Column columna2 = new Column("prueba2", "TEXT");
+            columna2.insertTEXTtoTable("Olaa K asEE");
+            Assert.Equals(columna1, columna2);
+
+            Column columna3 = new Column("prueba3", "INT");
+            columna3.insertINTtoTable(20022020);
+            Assert.Equals(columna3, columna2);
+
+        }
+    }
+}
