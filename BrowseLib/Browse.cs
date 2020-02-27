a7bb7de31e4748de1724f754160cc6e093c9be5e
Ander Pascual González <apascual047@ikasle.ehu.eus>
2020-02-27 14:12:10 +0200

    loadDatabase, saveDatabase

--- BrowseLib/Browse.cs
+++ BrowseLib/Browse.cs
@@ -4,45 +4,71 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BrowseLib
 {
-   public class Browse
+    public class Browse
     {
         public List<Database> databases;
 
+        // Constructor
         public Browse()
         {
             databases = new List<Database>();
         }
 
-        public void loadDatabases(string path)
+        // Add a database to the list databases
+        public void addDatabase(Database database)
         {
-            DirectoryInfo di = new DirectoryInfo(path);
-            DirectoryInfo[] directories = di.GetDirectories();
-            foreach (DirectoryInfo fi in directories)
-            {
-                databases.Add(new Database(fi.Name, "username", "password"));
-            }
-        }
-
-        public void saveDatabases(string path)
-        {
-            
-        }
-
-        public void addDatabase(Database database) {
             databases.Add(database);
         }
 
+        // Remove a database from the list databases
         public void deleteDatabase(Database database)
         {
             databases.Remove(database);
         }
 
-        public Database getDatabase(int num)
+        // Return the database in the given position
+        public Database getDatabase(int pos)
         {
-            return databases.ElementAt(num);
+            return databases.ElementAt(pos);
+        }
+
+        // Load the directories where there are databases
+        public void loadDatabases(string path)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+                DirectoryInfo[] directories = di.GetDirectories();
+                foreach (DirectoryInfo fi in directories)
+                {
+                    databases.Add(new Database(fi.Name, "username", "password"));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+        // Save the directories of each database in the list databases
+        public void saveDatabases(string path)
+        {
+            try
+            {
+                foreach (Database db in databases)
+                {
+                    Directory.CreateDirectory(Path.Combine(path, db.databaseName));
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
         }
     }
 }
