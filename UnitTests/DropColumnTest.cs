6ce727381b6b25c4c6998d7c7e6704ada06473cb
BorjaRey <borja300@gmail.com>
2020-03-27 20:46:52 +0100

    DropColumnTestAdded

---
+++ UnitTests/DropColumnTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowseLib;
+using System.Windows;
+
+namespace UnitTests
+{
+    /// A test or the creation of a table
+	
+
+    [TestClass]
+    public class TableTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+
+
+            Table table = new Table("MiTabla");
+            Column name = new Column("name", "TEXT");
+            name.insert("Borja");
+            name.insert("Unai");
+            table.addColumn(name);
+            table.dropColumn(name);
+
+        }
+
+
+
+    }
+}
