66fd0123aa2238e668b0e9ad668ab617ab35cfd3
Ander Pascual <anderpascual4@gmail.com>
2020-03-27 18:08:37 +0100

    Parse classes added

---
+++ BrowseLib/MiniSQL/Select.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BrowseLib.MiniSQL
+{
+    public class Select : MiniSQLQuery
+    {
+        public string Table = null;
+        public List<string> Columns = null;
+
+        public Select(string table, List<string> columns)
+        {
+            Table = table;
+            Columns = columns;
+        }
+
+        public string Execute(Database database)
+        {
+            return "";// database.Select(Table, Columns);
+        }
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -9,9 +9,23 @@ namespace ConsoleClient
 {
     class Program
     {
+        //minisql-tester.exe -i input-file.txt -o output-file.txt
+        enum Parameter { Unset, InputFile, OutputFile };
         static void Main(string[] args)
         {
+            string inputFile = "input-file.txt";
+            string outputFile = "output-file.txt";
+            Parameter lastParameter = Parameter.Unset;
+            foreach (string arg in args)
+            {
+                if (arg == "-i") lastParameter = Parameter.InputFile;
+                else if (arg == "-o") lastParameter = Parameter.OutputFile;
+                else if (lastParameter == Parameter.InputFile) inputFile = arg;
+                else if (lastParameter == Parameter.OutputFile) outputFile = arg;
+            }
 
+            Console.WriteLine("Input file: " + inputFile);
+            Console.WriteLine("Output file: " + outputFile);
         }
     }
 }
