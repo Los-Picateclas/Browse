a6158f629106a705b2cd5b61b1e94438dd035104
BorjaRey <borja300@gmail.com>
2020-03-27 19:38:35 +0100

    AddColumnTest added

---
+++ UnitTests/AddColumnTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrowseLib;
+using System.Windows;
+
+namespace UnitTests
+{
+    /// Adding a column
+	
+
+    [TestClass]
+    public class TableTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+
+
+            Table table = new Table("MiTabla");
+            Column name = new Column("name", "TEXT");
+            name.insert("Borja");
+            name.insert("Unai");
+            table.addColumn(name);
+
+        }
+
+
+
+    }
+}
