06f175c40dabc1a121c1883a13ab5448b88ee5eb
Javier Ortiz Díez <javierortizdiez@gmail.com>
2020-04-02 18:04:38 +0200

    CreateTableTest

--- UnitTests/CreateTableTest.cs
+++ UnitTests/CreateTableTest.cs
@@ -8,23 +8,21 @@ using System.Windows;
 namespace UnitTests
 {
     /// A test of the creation of a table
-	
 
     [TestClass]
     public class TableTest
     {
         [TestMethod]
-        public void Test()
+
+        public void CreateTable()
         {
-
-       
-               
-            Table table = new Table("MyTable");
-            
-
+            SQLParser parser = new SQLParser();
+            CreateTable sbres = (CreateTable)par.Parser("CREATE TABLE table1 (age INT);");
+            string[] a = new string[2];
+            a[0] = "age";
+            CreateTable sel = new CreateTable("table1", a);
+            Assert.AreEqual(sbres.GetType(), sel.GetType());
+            Assert.AreEqual(sbres.getTabla(), sel.getTabla());
         }
-
-
-
     }
 }
