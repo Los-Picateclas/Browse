56a0fe55df1cd63cc9ee2d97292afdf2a2cc6486
Ander Pascual <anderpascual4@gmail.com>
2020-04-02 12:18:12 +0200

    Insert and select in database

--- BrowseLib/Database.cs
+++ BrowseLib/Database.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BrowseLib.MiniSQL;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -96,6 +97,28 @@ namespace BrowseLib
             {
                 throw e;
             }
+        }
+
+        public string Select(string table, List<string> columns)
+        {
+            //Do whatever you have to do
+            return null;
+        }
+        public string Insert(string table, List<string> columns, List<string> values)
+        {
+            //Do whatever you have to do
+            return null;
+        }
+
+        public string ExecuteMiniSQLQuery(string query)
+        {
+            //Parse the query
+            MiniSQLQuery miniSQLQuery = MiniSQLParser.Parse(query);
+
+            if (miniSQLQuery == null)
+                return "Error";
+
+            return miniSQLQuery.Execute(this);
         }
     }
 }
