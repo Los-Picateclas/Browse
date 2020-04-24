bbd64f0697bd20172a55c32a643891dc5602fa8c
BorjaRey <borja300@gmail.com>
2020-04-24 17:25:50 +0200

    ConsoleClient reads lines

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,31 +1,37 @@
-﻿using System;
+﻿
+using System;
+using System.Diagnostics;
 using BrowseLib;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleClient
+namespace Programa
 {
     class Program
     {
-        //minisql-tester.exe -i input-file.txt -o output-file.txt
-        enum Parameter { Unset, InputFile, OutputFile };
+
         static void Main(string[] args)
         {
-            string inputFile = "input-file.txt";
-            string outputFile = "output-file.txt";
-            Parameter lastParameter = Parameter.Unset;
-            foreach (string arg in args)
-            {
-                if (arg == "-i") lastParameter = Parameter.InputFile;
-                else if (arg == "-o") lastParameter = Parameter.OutputFile;
-                else if (lastParameter == Parameter.InputFile) inputFile = arg;
-                else if (lastParameter == Parameter.OutputFile) outputFile = arg;
-            }
+            Database db = new Database("db1","user", "pass");
 
-            Console.WriteLine("Input file: " + inputFile);
-            Console.WriteLine("Output file: " + outputFile);
+            // Console.WriteLine(abc[0]);
+            //val = Console.ReadLine();
+
+            string linea = "";
+            System.IO.StreamReader file = new System.IO.StreamReader(@"input-file.txt");
+
+            while (linea != null)
+            {
+                linea = file.ReadLine();
+                if (linea != "" && linea != null)
+                {
+                    Stopwatch sw = new Stopwatch();
+                    sw.Start();
+                    string output = db.ExecuteMiniSQLQuery(linea) + "(";
+                    output += sw.Elapsed.TotalMilliseconds + ")";
+                    Console.WriteLine(output);
+                    sw.Stop();
+                }
+            }
+            Console.WriteLine("Querys Finished");
         }
     }
 }
