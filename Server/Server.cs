b7d38937ddd484a0f294140998c8ad2ab607d71c
Javier Ortiz Díez <javierortizdiez@outlook.es>
2020-06-01 13:00:37 +0200

    Class Server with the Port and Server

---
+++ Server/Server.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Server server = new Server();
+
+            //La Dirección IP y el Puerto
+
+            IPAddress localIP = IPAddress.Parse("127.0.0.1");  //El servidor
+            TcpListener listener = new TcpListener(IPAddress.Any, 1111); //El puerto
+            Console.WriteLine("Conexión con el servidor inicializada...");
+            Console.WriteLine("Esperando a lo que diga cliente...");
+        }
+    }
+}
