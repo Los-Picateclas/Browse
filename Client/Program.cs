e03948b07896ac011af4e2891e8b23e6d9fb920f
Unai Foncea <unaifon@hotmail.com>
2020-06-02 12:21:54 +0200

    created client changes in server now they make the connection and the server shows a string sent by the client.

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //We need a socket and a IP adrees to connect to the server so we create it.
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            socket.Connect(direction);
+            Console.WriteLine("Connected");
+            Console.WriteLine("Server is listening...");
+
+            string info = Console.ReadLine();
+            //We need to transform the info to bytes so we can send it to the server
+            byte[] infoToSocket = Encoding.Default.GetBytes(info);
+            socket.Send(infoToSocket, 0, infoToSocket.Length, 0);
+            Console.WriteLine("Sending...");
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
+
+
+        }
+    }
+}
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace Server
 {
     class Server
     {
-        static void Main(string[] args)
-        {
-            Console.Clear();
-            Server server = new Server();
+        public static void Main(string[] args) {
+            //We will use sockets to make the server. 
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //Now we specify the IP and the port. 127.0.0.1 is our local pc direction
+            IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            socket.Bind(direction);
+            socket.Listen(1); //We only accept one connection for our socket.
+            Console.WriteLine("Waiting for the conection...");
+            Socket listen = socket.Accept(); //This new socket will return the client response
+            Console.WriteLine("Connected");
 
-            //La Dirección IP y el Puerto
-
-            IPAddress localIP = IPAddress.Parse("127.0.0.1");  //El servidor
-            TcpListener listener = new TcpListener(IPAddress.Any, 1111); //El puerto
-            Console.WriteLine("Conexión con el servidor inicializada...");
-            Console.WriteLine("Esperando a lo que diga cliente...");
+            //We can only use bytes to transfer the information.
+            byte[] info = new byte[255];
+            int receive = listen.Receive(info, 0, info.Length, 0);
+            Console.WriteLine("Client says: " + Encoding.Default.GetString(info)); //we tranform the info to a string
+            socket.Close();
+            Console.WriteLine("Press any key to finish");
+            Console.ReadKey();
         }
-    }
+
+
+       
+
+
+        }
+    
 }
+
+
+
