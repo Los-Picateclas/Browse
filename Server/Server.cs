b05b752f02369d3f96e0cb951e5842e1788e337b
Unai Foncea <unaifon@hotmail.com>
2020-06-03 13:10:38 +0200

    added infinite loop in server and the client will close when we type exit Now you are able to type text and send it from the client until you write exit and the server is continuously showing it

--- Server/Server.cs
+++ Server/Server.cs
@@ -14,18 +14,22 @@ namespace Server
             //Now we specify the IP and the port. 127.0.0.1 is our local pc direction
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Bind(direction);
-            socket.Listen(1); //We only accept one connection for our socket.
-            Console.WriteLine("Waiting for a client...");
-            Socket listen = socket.Accept(); //This new socket will return the client response
-            Console.WriteLine("Connected");
 
+                socket.Listen(1); //We only accept one connection for our socket.
+            Console.WriteLine("Waiting for a client...");
+                Socket listen = socket.Accept(); //This new socket will return the client response
+            Console.WriteLine("Connected");
             //We can only use bytes to transfer the information.
             byte[] info = new byte[255];
-            int receive = listen.Receive(info, 0, info.Length, 0);
-            Console.WriteLine("Client says: " + Encoding.Default.GetString(info)); //we tranform the info to a string
-            socket.Close();
-            Console.WriteLine("Press any key to finish");
-            Console.ReadKey();
+            while (true)
+            {
+                Array.Clear(info, 0, 255);
+                int receive = listen.Receive(info, 0, info.Length, 0);
+                string infoString = Encoding.Default.GetString(info); //we tranform the info to a string
+                Console.WriteLine("Client says: " + infoString);
+                socket.Close();
+            }
+           
         }
 
         }
