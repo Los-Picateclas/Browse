f3f3cc206178f5413ebd4f07677975a95ef1d766
Javier Ortiz Díez <javierortizdiez@outlook.es>
2020-06-03 18:55:35 +0200

    TRying to initialize Database in client

--- Client/Client.cs
+++ Client/Client.cs
@@ -3,11 +3,20 @@ using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using BrowseLib;
 
 namespace Client
 {
     class Client
     {
+        public Database db;       
+        public void initialize()
+        {
+            //Initilize the database
+            db = new Database("mydatabase", "user", "password");
+
+        }
+
         static void Main(string[] args)
         {
             //We need a socket and a IP adrees to connect to the server so we create it.
@@ -15,20 +24,23 @@ namespace Client
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Connect(direction);
             Console.WriteLine("Connected to the server");
+            Console.WriteLine("Write exit in case you want to end");
             Console.WriteLine(" Introduce the SQL query...");
             string text = Console.ReadLine();
-            //name of the txt archive 
+
+            //Name of the txt archive 
             while (text != "exit")
             {
                 Console.WriteLine("Sending information to the server...");
+
                 //We need to transform the info to bytes so we can send it to the server
                 byte[] byteText = Encoding.Default.GetBytes(text);
                 socket.Send(byteText, 0, byteText.Length, 0);
-                Console.WriteLine("done");
+                Console.WriteLine("Done");
+                Console.WriteLine();
                 text = Console.ReadLine();
             }
 
-        
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -11,21 +11,22 @@ namespace Server
         {
             //We will use sockets to make the server. 
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
             //Now we specify the IP and the port. 127.0.0.1 is our local pc direction
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Bind(direction);
-
-                socket.Listen(1); //We only accept one connection for our socket.
+            socket.Listen(1); //We only accept one connection for our socket.
             Console.WriteLine("Waiting for a client...");
-                Socket listen = socket.Accept(); //This new socket will return the client response
+            Socket listen = socket.Accept(); //This new socket will return the client response
             Console.WriteLine("Connected");
-            //We can only use bytes to transfer the information.
+            //We can only use bytes to transfer the information
             byte[] info = new byte[255];
+
             while (true)
             {
                 Array.Clear(info, 0, 255);
                 int receive = listen.Receive(info, 0, info.Length, 0);
-                string infoString = Encoding.Default.GetString(info); //we tranform the info to a string
+                string infoString = Encoding.Default.GetString(info); //We tranform the info to string
                 Console.WriteLine("Client says: " + infoString);
                 socket.Close();
             }
