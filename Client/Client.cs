9f715884abc62a3fc4df1d689d5b9ce6e058088b
Javier Ortiz Díez <javierortizdiez@outlook.es>
2020-06-08 20:56:58 +0200

    If contador added and insert

--- Client/Client.cs
+++ Client/Client.cs
@@ -21,32 +21,40 @@ namespace Client
         static void Main(string[] args)
         {
             //We need a socket and a IP adrees to connect to the server so we create it
+            int contador = 0;
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Connect(direction);
             Console.WriteLine("Connected to the server");
             Console.WriteLine("Write exit in case you want to end");
             Console.WriteLine(" Introduce the SQL query...");
+
             string text = Console.ReadLine();
+            String query = "INSERT INTO db.mytable(id,username,password) VALUES (@" + text + ")";
+            byte[] byteQuery = Encoding.Default.GetBytes(query);
 
             //Name of the txt archive 
             while (text != "exit")
             {
+                if (contador != 0)
+                {
+                    Console.WriteLine(" Introduce the SQL query...");
+                    text = Console.ReadLine();
+                    query = "INSERT INTO db.mytable(id,username,password) VALUES (@" + text + ")";
+                    byteQuery = Encoding.Default.GetBytes(query);
+                }
                 Console.WriteLine("Sending information to the server...");
-
+                contador = 1;
                 //We need to transform the info to bytes so we can send it to the server
                 byte[] byteText = Encoding.Default.GetBytes(text);
+                //socket.Send(byteText, 0, byteText.Length, 0);
                 socket.Send(byteText, 0, byteText.Length, 0);
                 Console.WriteLine("Done");
                 Console.WriteLine();
-                text = Console.ReadLine();
+                //text = Console.ReadLine();
+                text = "";
 
             }
-
-            //Insert
-            String query = "INSERT INTO db.mytable(id,username,password) VALUES (@1,@javier,@password)";
-            byte[] byteQuery = Encoding.Default.GetBytes(query);
-            socket.Send(byteQuery, 0, byteQuery.Length, 0);
 
         }
     }
