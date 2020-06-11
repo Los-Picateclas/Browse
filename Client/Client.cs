6558aedb34ac2781318897b8d3f839600d0565e2
javierortdi <javierortizdiez@gmail.com>
2020-06-11 22:53:26 +0200

    Database initialized with the permissions Insert, Select and Update works

--- Client/Client.cs
+++ Client/Client.cs
@@ -9,36 +9,76 @@ using BrowseLib.MiniSQL;
 namespace Client
 {
     class Client
-    {    
+    {
+        public void sendQuery(String text, Socket socket)
+        {
+
+            Console.WriteLine("Sending information to the server...");
+            byte[] byteText = Encoding.Default.GetBytes(text);
+            socket.Send(byteText, 0, byteText.Length, 0);
+            Console.WriteLine("Done");
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            //We need a socket and a IP adrees to connect to the server so we create it
+
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Connect(direction);
             Console.WriteLine("Connected to the server");
-            Console.WriteLine("Write exit in case you want to end");
-            Console.WriteLine(" Introduce the SQL query...");
-            string text = Console.ReadLine();
+            string text = "";
+            string option = "";
+            Client cliente = new Client();
 
-            //Name of the txt archive 
-            while (text != "exit")
+            while (option != "exit")
             {
-                Console.WriteLine("Sending information to the server...");
 
-                //We need to transform the info to bytes so we can send it to the server
-                byte[] byteText = Encoding.Default.GetBytes(text);
-                socket.Send(byteText, 0, byteText.Length, 0);
-                Console.WriteLine("Done");
-                Console.WriteLine();
-                text = Console.ReadLine();
+                Console.WriteLine("Introduce what do you want to do...");
+                Console.WriteLine("1: Insert");
+                Console.WriteLine("2: Select");
+                Console.WriteLine("3: Update");
+                Console.WriteLine("4: Delete");
+                Console.WriteLine("Write exit in case you want to end");
+                option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("Introduce nombre");
+                        text = Console.ReadLine();
+                        String insertQuery = "INSERT INTO mytable VALUES (" + text + ");";
+                        cliente.sendQuery(insertQuery, socket);
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Seleccionamos todo de la tabla");
+                        String selectQuery = "SELECT * FROM mytable;";
+                        cliente.sendQuery(selectQuery, socket);
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Case 3");
+                        cliente.sendQuery(text, socket);
+                        break;
+
+                    case "4":
+                        Console.WriteLine("Selecciona el nombre de la tabla a borrar");
+                        text = Console.ReadLine();
+                        String deleteQuery = "DELETE FROM mytable WHERE (" + text + ");";
+                        cliente.sendQuery(deleteQuery, socket);
+                        break;
+
+                    case "exit":
+
+                        socket.Close();
+                        break;
+
+                    default:
+                        Console.WriteLine("Incorrect value");
+                        break;
+                }
             }
-
-            //Insert
-            MiniSQLQuery query = MiniSQLParser.Parse("INSERT INTO Person VALUES (@" + text + ")");
-            Insert insertQuery = query as Insert;
-            //byte[] byteQuery = Encoding.Default.GetBytes(query);
-            //socket.Send(insertQuery, 0, insertQuery.Length, 0);
         }
     }
 }
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using BrowseLib;
 using BrowseLib.MiniSQL;
@@ -9,19 +10,45 @@ namespace Server
 {
     class Server
     {
+        public static Database db;
+
         public void initialize()
         {
-            //Initilize the database
-            //Database db = new Database("mydatabase", "user", "password");
-            BrowseLib.Database database = new Database("mydatabase", "user", "password");
-            //Initialize the table
+
+            //Create the table
             Table tb = new Table("mytable");
 
-            database.ExecuteMiniSQLQuery("query");
-        }
+            //Create the columns
+            Column cm = new Column("mycolumn", "text");
 
+            //Create profile and give permissions
+            Profile prof = new Profile("admin");
+            TablePermission tbp = new TablePermission(tb.getName());
+            tbp.addPrivilege(Privileges.DELETE);
+            tbp.addPrivilege(Privileges.INSERT);
+            tbp.addPrivilege(Privileges.SELECT);
+            tbp.addPrivilege(Privileges.UPDATE);
+            prof.addTablePermission(tbp);
+
+            //Give privileges
+            User user = new User("admin", "admin", prof);
+           
+            //Create the database
+            db = new Database("mydatabase", user.getName(), user.getPassword());
+
+            //Initialize the database
+            db.addUser(user.getName(), user.getPassword(), prof.getName());
+            db.setActualUser(user.getName());
+            tb.addColumn(cm);
+            db.addTable(tb);
+
+        }
         public static void Main(string[] args)
         {
+
+            Server server = new Server();
+            server.initialize();
+
             //We will use sockets to make the server. 
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
@@ -37,10 +64,16 @@ namespace Server
 
             while (true)
             {
-                Array.Clear(info, 0, 255);
+
+                Array.Clear(info, 0, info.Length);
                 int receive = listen.Receive(info, 0, info.Length, 0);
                 string infoString = Encoding.Default.GetString(info); //We tranform the info to string
                 Console.WriteLine("Client says: " + infoString);
+                int found = infoString.IndexOf("\0");
+                String s = infoString.Substring(0, found);
+                String valor = db.ExecuteMiniSQLQuery(s);
+                //Console.WriteLine(valor);
+                Console.WriteLine("Server says: " + valor);
                 socket.Close();
 
             }           
