8d0d8c2bdcb7cab0abcb53b2e6fd231d2c908621
Javier Ortiz Díez <javierortizdiez@outlook.es>
2020-06-12 11:52:07 +0200

    Detaild and some problems fixed

--- Client/Client.cs
+++ Client/Client.cs
@@ -10,6 +10,7 @@ namespace Client
 {
     class Client
     {
+        //Send the different type queries
         public void sendQuery(String text, Socket socket)
         {
 
@@ -23,6 +24,7 @@ namespace Client
         static void Main(string[] args)
         {
 
+            //Create socket and conexion with server
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Connect(direction);
@@ -33,7 +35,7 @@ namespace Client
 
             while (option != "exit")
             {
-
+                //Create the menu
                 Console.WriteLine("Introduce what do you want to do...");
                 Console.WriteLine("1: Insert");
                 Console.WriteLine("2: Select");
@@ -45,32 +47,32 @@ namespace Client
                 switch (option)
                 {
                     case "1":
-                        Console.WriteLine("Introduce nombre");
+                        Console.WriteLine("Introduce the name of the tuple you want to add");
                         text = Console.ReadLine();
                         String insertQuery = "INSERT INTO mytable VALUES (" + text + ");";
                         cliente.sendQuery(insertQuery, socket);
                         break;
 
                     case "2":
-                        Console.WriteLine("Seleccionamos todo de la tabla");
+                        Console.WriteLine("All the tables selected");
                         String selectQuery = "SELECT * FROM mytable;";
                         cliente.sendQuery(selectQuery, socket);
                         break;
 
                     case "3":
-                        Console.WriteLine("Case 3");
-                        cliente.sendQuery(text, socket);
+                        Console.WriteLine("Change the name of the tuples");
+                        text = Console.ReadLine();
+                        String updateQuery = "UPDATE mytable SET * WHERE (" + text + ");";
+                        cliente.sendQuery(updateQuery, socket);
                         break;
 
                     case "4":
-                        Console.WriteLine("Selecciona el nombre de la tabla a borrar");
-                        text = Console.ReadLine();
-                        String deleteQuery = "DELETE FROM mytable WHERE (" + text + ");";
+                        Console.WriteLine("Values of the table deleted");
+                        String deleteQuery = "DELETE * FROM mytable;";
                         cliente.sendQuery(deleteQuery, socket);
                         break;
 
                     case "exit":
-
                         socket.Close();
                         break;
 
--- Server/Server.cs
+++ Server/Server.cs
@@ -46,6 +46,7 @@ namespace Server
         public static void Main(string[] args)
         {
 
+            //Create the server and initialize
             Server server = new Server();
             server.initialize();
 
@@ -55,10 +56,15 @@ namespace Server
             //Now we specify the IP and the port. 127.0.0.1 is our local pc direction
             IPEndPoint direction = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
             socket.Bind(direction);
-            socket.Listen(1); //We only accept one connection for our socket.
+
+            //We only accept one connection for our socket
+            socket.Listen(1); 
             Console.WriteLine("Waiting for a client...");
-            Socket listen = socket.Accept(); //This new socket will return the client response
+
+            //This new socket will return the client response
+            Socket listen = socket.Accept(); 
             Console.WriteLine("Connected");
+
             //We can only use bytes to transfer the information
             byte[] info = new byte[255];
 
@@ -67,12 +73,17 @@ namespace Server
 
                 Array.Clear(info, 0, info.Length);
                 int receive = listen.Receive(info, 0, info.Length, 0);
-                string infoString = Encoding.Default.GetString(info); //We tranform the info to string
+
+                //We tranform the info to string
+                string infoString = Encoding.Default.GetString(info); 
                 Console.WriteLine("Client says: " + infoString);
+
+                //The server doesn´t receive all the 0s frome the query
                 int found = infoString.IndexOf("\0");
                 String s = infoString.Substring(0, found);
+
+                //Execute MiniSQLQuery and do the Parser
                 String valor = db.ExecuteMiniSQLQuery(s);
-                //Console.WriteLine(valor);
                 Console.WriteLine("Server says: " + valor);
                 socket.Close();
 
